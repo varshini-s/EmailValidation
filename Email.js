3c7a93c1862c8ab5e78c2acb37a0d1bc614fbe2f
varshini <dreamvarshini@gmail.com>
2021-10-07 22:18:58 +0530

    MODIFIED: ensuring . and mandatory third part of mailid

--- Email.js
+++ Email.js
@@ -1,4 +1,4 @@
-const EMAIL_PATTERN = RegExp("^[a-z0-9]+@[a-z0-9]+$");
+const EMAIL_PATTERN = RegExp("^[a-z0-9]+@[a-z0-9]+[.][a-z]{2,4}$");
 
 let validMails = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
     "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"];
