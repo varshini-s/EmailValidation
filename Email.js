bfb9f88897c601465c2718e45af4a995af3d544c
varshini <dreamvarshini@gmail.com>
2021-10-07 22:44:12 +0530

    MODIFIED: changing regex for optional part of mail id

--- Email.js
+++ Email.js
@@ -1,4 +1,4 @@
-const EMAIL_PATTERN = RegExp("^[a-z0-9]+([+[.]_-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}$");
+const EMAIL_PATTERN = RegExp("^[a-z0-9]+([+._-][a-z0-9]+){0,1}@[a-z0-9]+[.][a-z]{2,4}([.][a-z]{2,4}){0,1}$");
 
 let validMails = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
     "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"];
@@ -9,7 +9,7 @@ let invalidMails = ["abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com.com", "
 
 
 let validateAllValidMails=validMails.every(email=>EMAIL_PATTERN.test(email))
-console.log("Is all  valid mail id verified:"+validateAllValidMails)
+console.log("Has all  valid mail id verified as valid mail id:"+validateAllValidMails)
 
-let validateAllInValidMails=validMails.every(email=>!(EMAIL_PATTERN.test(email)))
-console.log("Is all  Invalid mail id verified:"+validateAllInValidMails)
+let validateAllInValidMails=invalidMails.every(email=>EMAIL_PATTERN.test(email)==false)
+console.log("Has all  Invalid mail id verified as invalid mail id:"+validateAllInValidMails)
