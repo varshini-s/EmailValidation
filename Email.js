6d437be21e4bfb7fc4aac3798e56eefafd34702d
varshini <dreamvarshini@gmail.com>
2021-10-07 22:10:30 +0530

    ADDED: validating first mandatory part of mailID

---
+++ Email.js
@@ -0,0 +1,15 @@
+const EMAIL_PATTERN = RegExp("^[a-z0-9]+$");
+
+let validMails = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
+    "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"];
+
+let invalidMails = ["abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com"
+    , "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", ".abc@gmail.com.aa.au"];
+
+
+
+let validateAllValidMails=validMails.every(email=>EMAIL_PATTERN.test(email))
+console.log("Is all  valid mail id verified:"+validateAllValidMails)
+
+let validateAllInValidMails=validMails.every(email=>!(EMAIL_PATTERN.test(email)))
+console.log("Is all  Invalid mail id verified:"+validateAllInValidMails)
